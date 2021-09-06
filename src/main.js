ae609868858210d37cf55699bb32f2f447a0f209
Nohely Badillo <62182593+nohelyx-bc@users.noreply.github.com>
2021-09-06 01:16:45 -0500

    applying createElement
    attempts were added for function of routing, textContent and createElement were used to create the first routes

--- src/main.js
+++ src/main.js
@@ -1,3 +1,9 @@
-import { myFunction } from './lib/index.js';
+import { Home } from "./components/home.js"
+import { Register } from "./components/register.js"
+import { Login } from "./components/login.js"
 
-myFunction();
+const routes = {
+    '/': home, 
+    '/register': Register
+    'login': Login
+}
