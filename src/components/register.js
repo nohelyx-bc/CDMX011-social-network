ffc0cf6115577463d66d51c1e0d46d415b98434d
Nohely Badillo <62182593+nohelyx-bc@users.noreply.github.com>
2021-09-07 14:03:45 -0500

    add elements in register.js and basic styles

--- src/components/register.js
+++ src/components/register.js
@@ -1,16 +1,39 @@
-/* eslint-disable no-undef */
-// eslint-disable-next-line import/no-cycle
 
 import { onNavigate } from '../main.js';
 
 export const Register = () => {
+
   const HomeDiv = document.createElement('div');
+  const inputName = document.createElement('input');
+  const inputEmail = document.createElement('input');
+  const inputPassword = document.createElement('input');
+  const inputConfirmPassword = document.createElement('input');
   const buttonRegister = document.createElement('button');
-  
+  const buttonRegisterGoogle = document.createElement('button');
+  const linkLogin = document.createElement('a');
+
+  HomeDiv.className = 'home';
+
+  inputName.placeholder = 'Nombre';
+  inputEmail.placeholder = 'Email';
+  inputPassword.placeholder = 'Constraseña';
+  inputConfirmPassword.placeholder = 'Confirma tu contraseña';
   buttonRegister.textContent = 'Registrar';
+  buttonRegisterGoogle.textContent = 'Regístrate con Google';
+  linkLogin.textContent = '¿Ya tienes cuenta? Ingresa';
+
+  HomeDiv.appendChild(inputName);
+  HomeDiv.appendChild(inputEmail);
+  HomeDiv.appendChild(inputPassword);
+  HomeDiv.appendChild(inputConfirmPassword);
+  HomeDiv.appendChild(buttonRegister);
+  HomeDiv.appendChild(buttonRegisterGoogle);
+  HomeDiv.appendChild(linkLogin);
+
 
   buttonRegister.addEventListener('click', () => onNavigate('/'));
-  HomeDiv.appendChild(buttonRegister);
+  buttonRegisterGoogle.addEventListener('click', () => onNavigate('/'));
+  linkLogin.addEventListener('click', () => onNavigate('/'))
 
   return HomeDiv;
 };
