62c902f187aaa94155adb38a505b66729ac8d602
Nohely Badillo <62182593+nohelyx-bc@users.noreply.github.com>
2021-09-08 00:07:28 -0500

    added styles based on our prototype

--- src/components/home.js
+++ src/components/home.js
@@ -3,6 +3,7 @@ import { onNavigate } from '../main.js';
 
 export const Home = () => {
   const HomeDiv = document.createElement('div');
+  const homeBox = document.createElement('div');
   const inputUser = document.createElement('input');
   const inputPassword = document.createElement('input');
   const buttonLogin = document.createElement('button');
@@ -12,13 +13,18 @@ export const Home = () => {
   const linkRegister = document.createElement('a');
 
   HomeDiv.className = 'home';
+  homeBox.className = 'home_box';
+  buttonLogin.className = 'login_style';
+
   inputUser.placeholder = 'Nombre';
   inputPassword.placeholder = 'Contraseña';
   buttonLogin.textContent = 'Ingresar';
   buttonLoginGoogle.textContent = 'Ingresa a través de Google';
   buttonLoginGithub.textContent = 'Ingresa a través de GitHub';
   linkPassword.textContent = '¿Olvidaste tu contraseña?';
-  linkRegister.textContent = '¿No tienes cuenta? Regístrate'
+  linkPassword.href = '#';
+  linkRegister.textContent = '¿No tienes cuenta? Regístrate';
+  linkRegister.href = '#';
 
 
   HomeDiv.appendChild(inputUser);
@@ -26,8 +32,9 @@ export const Home = () => {
   HomeDiv.appendChild(buttonLogin);
   HomeDiv.appendChild(buttonLoginGithub);
   HomeDiv.appendChild(buttonLoginGoogle);
-  HomeDiv.appendChild(linkPassword);
-  HomeDiv.appendChild(linkRegister);
+  HomeDiv.appendChild(homeBox);
+  homeBox.appendChild(linkPassword);
+  homeBox.appendChild(linkRegister);
 
 
   buttonLogin.addEventListener('click', () => onNavigate('/'));
--- src/components/register.js
+++ src/components/register.js
@@ -4,15 +4,19 @@ import { onNavigate } from '../main.js';
 export const Register = () => {
 
   const HomeDiv = document.createElement('div');
+  const homeBox = document.createElement('div');
   const inputName = document.createElement('input');
   const inputEmail = document.createElement('input');
   const inputPassword = document.createElement('input');
   const inputConfirmPassword = document.createElement('input');
   const buttonRegister = document.createElement('button');
   const buttonRegisterGoogle = document.createElement('button');
+  const buttonRegisterGithub = document.createElement('button');
   const linkLogin = document.createElement('a');
 
   HomeDiv.className = 'home';
+  homeBox.className = 'home_box';
+  buttonRegister.className = 'login_style';
 
   inputName.placeholder = 'Nombre';
   inputEmail.placeholder = 'Email';
@@ -20,7 +24,9 @@ export const Register = () => {
   inputConfirmPassword.placeholder = 'Confirma tu contraseña';
   buttonRegister.textContent = 'Registrar';
   buttonRegisterGoogle.textContent = 'Regístrate con Google';
+  buttonRegisterGithub.textContent = 'Regístrate con GitHub';
   linkLogin.textContent = '¿Ya tienes cuenta? Ingresa';
+  linkLogin.href = '#';
 
   HomeDiv.appendChild(inputName);
   HomeDiv.appendChild(inputEmail);
@@ -28,7 +34,9 @@ export const Register = () => {
   HomeDiv.appendChild(inputConfirmPassword);
   HomeDiv.appendChild(buttonRegister);
   HomeDiv.appendChild(buttonRegisterGoogle);
-  HomeDiv.appendChild(linkLogin);
+  HomeDiv.appendChild(buttonRegisterGithub);
+  HomeDiv.appendChild(homeBox);
+  homeBox.appendChild(linkLogin);
 
 
   buttonRegister.addEventListener('click', () => onNavigate('/'));
