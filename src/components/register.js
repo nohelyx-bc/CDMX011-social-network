6023bda65ac91f5b4b484a1a38065d2b7a9d037f
Alba <alba.lp95@gmail.com>
2021-09-10 01:05:05 -0500

    Modifying styles and adding comments to the code

--- src/components/register.js
+++ src/components/register.js
@@ -1,8 +1,7 @@
-
 import { onNavigate } from '../main.js';
 
+// On the route home we insert the html elements which be shown in the DOM
 export const Register = () => {
-
   const HomeDiv = document.createElement('div');
   const homeBox = document.createElement('div');
   const inputName = document.createElement('input');
@@ -13,11 +12,11 @@ export const Register = () => {
   const buttonRegisterGoogle = document.createElement('button');
   const buttonRegisterGithub = document.createElement('button');
   const linkLogin = document.createElement('a');
-
+  // Assign classNames to the elements so we can manipulate it with css
   HomeDiv.className = 'home';
   homeBox.className = 'home_box';
   buttonRegister.className = 'login_style';
-
+  // Naming the elements
   inputName.placeholder = 'Nombre';
   inputEmail.placeholder = 'Email';
   inputPassword.placeholder = 'Constraseña';
@@ -27,7 +26,7 @@ export const Register = () => {
   buttonRegisterGithub.textContent = 'Regístrate con GitHub';
   linkLogin.textContent = '¿Ya tienes cuenta? Ingresa';
   linkLogin.href = '#';
-
+  // Inserting the elements into the HomeDiv
   HomeDiv.appendChild(inputName);
   HomeDiv.appendChild(inputEmail);
   HomeDiv.appendChild(inputPassword);
@@ -35,13 +34,12 @@ export const Register = () => {
   HomeDiv.appendChild(buttonRegister);
   HomeDiv.appendChild(buttonRegisterGoogle);
   HomeDiv.appendChild(buttonRegisterGithub);
-  HomeDiv.appendChild(homeBox);
-  homeBox.appendChild(linkLogin);
-
-
+  HomeDiv.appendChild(linkLogin);
+  // Adding the events to the buttons and links
   buttonRegister.addEventListener('click', () => onNavigate('/'));
   buttonRegisterGoogle.addEventListener('click', () => onNavigate('/'));
-  linkLogin.addEventListener('click', () => onNavigate('/'))
+  buttonRegisterGithub.addEventListener('click', () => onNavigate('/'));
+  linkLogin.addEventListener('click', () => onNavigate('/'));
 
   return HomeDiv;
 };
--- src/main.js
+++ src/main.js
@@ -1,14 +1,17 @@
 import { Home } from './components/home.js';
 import { Register } from './components/register.js';
 
+/* Reasign the name of the div in the html file who'll contain *
+all the elements (with createElement and appenChild) */
 const rootDiv = document.getElementById('root');
 
+/* We asign to the routes the component which are home and register */
 const routes = {
   '/': Home,
   '/register': Register,
 };
 
-// Funcion para insertar el nodo, html con appendChild
+// Function to insert the node, html con appendChild
 export const onNavigate = (pathname) => {
   window.history.pushState(
     {},
@@ -23,15 +26,14 @@ export const onNavigate = (pathname) => {
   rootDiv.appendChild(routes[pathname]());
 };
 
-//
-
 window.onpopstate = () => {
   while (rootDiv.firstChild) {
     rootDiv.removeChild(rootDiv.firstChild);
   }
   rootDiv.appendChild(routes[window.location.pathname]());
 };
-
+// Again we declare a new variable who'll contain our routes
 const component = routes[window.location.pathname];
-
+/* We insert the routes to be shown in the DOM.
+ Remember we call component() executing it, because it's a function */
 rootDiv.appendChild(component());
